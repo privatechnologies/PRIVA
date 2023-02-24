159598c3c2217f6847ef08cc661c1fb65fd4321a
privatechnologies <124781676+privatechnologies@users.noreply.github.com>
2023-02-24 20:13:24 +0530

    Add files via upload

---
+++ priva-technologies-main/login.js
@@ -0,0 +1,10 @@
+$('.register-show').click(function() {
+    $('.Register-form').show();
+    $('.Login-form').hide();
+})
+
+
+$('.login-show').click(function() {
+    $('.Login-form').show();
+    $('.Register-form').hide();
+})
